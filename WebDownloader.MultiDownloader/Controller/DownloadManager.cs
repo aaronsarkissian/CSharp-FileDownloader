8c2c32d7f8c5486cd6a434d4a3b18f9a272733ad
Aaron Sarkissian <aaronsarkissian@gmail.com>
2019-04-05 12:19:52 +0400

    Add the main parts

---
+++ WebDownloader.MultiDownloader/Controller/DownloadManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace WebDownloader.MultiDownloader.Controller
+{
+
+    public class DownloadManager
+    {
+        private static int count = 1;
+        private string url;
+        private string path;
+        private ListView listView;
+        public ProgressBar progress;
+        public WebClient client;
+
+        public DownloadManager(string url, string path, ListView listView)
+        {
+            this.url = url;
+            this.path = path;
+            this.listView = listView;
+        }
+
+        private WebClient Download()
+        {
+            WebClient wc = new WebClient();
+            Uri link = new Uri(url);
+
+            string filename = Path.GetFileName(link.LocalPath);
+
+            wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(WebClient_DownloadProgressChanged);
+            wc.DownloadFileAsync(link, path + Path.DirectorySeparatorChar + filename);
+            progress = new ProgressBar();
+            Item item = new Item() { Count = count++, MyProgress = progress };
+            listView.Items.Add(item);
+            return wc;
+        }
+
+        public void Start()
+        {
+            client = Download();
+        }
+
+        public void CancelDownload()
+        {
+            client.CancelAsync();
+        }
+
+        private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progress.Dispatcher.Invoke(() =>
+                    progress.Value = e.ProgressPercentage,
+                    DispatcherPriority.Background
+            );
+        }
+    }
+
+    public class Item
+    {
+        public int Count { get; set; }
+        public ProgressBar MyProgress { get; set; }
+    }
+}
+
+// Big images for testing
+// https://upload.wikimedia.org/wikipedia/commons/0/0a/Parc_de_Versailles%2C_Fontaine_du_Point_du_Jour_02.jpg
+// https://upload.wikimedia.org/wikipedia/commons/e/e6/Clocktower_Panorama_20080622_20mb.jpg
--- WebDownloader.MultiDownloader/MainWindow.xaml.cs
+++ WebDownloader.MultiDownloader/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using WebDownloader.MultiDownloader.Controller;
+using System.Windows.Data;
 
 namespace WebDownloader.MultiDownloader
 {
@@ -20,9 +13,45 @@ namespace WebDownloader.MultiDownloader
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string path = string.Empty;
+        DownloadManager downloader; // Remove this
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Directory_button_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            DialogResult result = folderBrowser.ShowDialog();
+            if(result == System.Windows.Forms.DialogResult.OK)
+            {
+                path = folderBrowser.SelectedPath;
+                directory_button.Background = Brushes.Green;
+            }
+        }
+
+        private void Download_Click(object sender, RoutedEventArgs e)
+        {
+            var url = file_url.Text;
+            var dirPath = path;
+
+            // TODO: check for some edge cases...
+
+            DownloadManager downloader = new DownloadManager(url, dirPath, fileList);
+            downloader.Start();
+            // Give each button an unique id, so you can cancel them later
+            //CancelButton.onclick = () =>
+            //{
+            //    downloader.CancelDownload;
+            //}
+        }
+
+        // Fix this - it only cancels the last item!
+        private void Cancel_button_Click(object sender, RoutedEventArgs e)
+        {
+            downloader.CancelDownload();
         }
     }
 }
